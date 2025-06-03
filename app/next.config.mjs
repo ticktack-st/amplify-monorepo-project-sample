7621dc22d5fd388abe0cbe13571ad5a40d808dba
ticktack-st <moglandwrapper@gmail.com>
2025-06-03 18:58:30 +0900

    del: next.configのruntimeConfigはNG

--- app/next.config.mjs
+++ app/next.config.mjs
@@ -3,17 +3,6 @@ const nextConfig = {
   eslint: {
     dirs: ['src/'],
   },
-  serverRuntimeConfig: {
-    cognito: {
-      Auth: {
-        Cognito: {
-          userPoolId: 'ap-northeast-1_fVGVH0WsZ',
-          userPoolClientId: '1d27eld0o3b5onl981729npi0s',
-          region: 'ap-northeast-1',
-        },
-      },
-    },
-  },
 }
 
 export default nextConfig
