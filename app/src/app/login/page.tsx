cf006e1f9a39623c492ec0776d0eb175ac4d48f7
ticktack-st <moglandwrapper@gmail.com>
2025-06-03 13:45:53 +0900

    add: login page

---
+++ app/src/app/login/page.tsx
@@ -0,0 +1,9 @@
+import 'aws-amplify/auth/enable-oauth-listener'
+
+export default function Page() {
+  return (
+    <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+      ログインしてください
+    </div>
+  )
+}
