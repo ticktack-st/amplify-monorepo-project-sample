c3c422a3beb6453de02a91227e19af0912948ee2
ticktack-st <moglandwrapper@gmail.com>
2025-06-10 20:15:26 +0900

    add: cypressコンテナ用の設定ファイル

---
+++ app/cypress/test/eslint.config.mjs
@@ -0,0 +1,47 @@
+import eslint from '@eslint/js'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+
+export default [
+  // global設定
+  {
+    ignores: ['node_modules/**/*', 'eslint.config.mjs'],
+  },
+  {
+    // @typescript-eslintに関する設定
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...globals.es2024,
+        ...globals.builtin,
+      },
+      parser: tseslint.parser,
+      parserOptions: {
+        projectService: true,
+        project: './tsconfig.eslint.json',
+        tsconfigRootDir: import.meta.dirname,
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+  },
+
+  // ルール適用
+  {
+    // 基本ルール適用
+    files: ['src/**/*.{js,mjs,jsx,ts,tsx}'],
+    plugins: {
+      eslint: eslint,
+      tseslint: tseslint,
+    },
+    rules: {
+      ...eslint.configs.recommended.rules,
+      ...tseslint.configs.strict.rules,
+      'no-undef': 'off',
+    },
+  },
+]
