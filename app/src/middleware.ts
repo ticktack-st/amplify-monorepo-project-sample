632a15f8ae0598a1e5a3283c787071db41bd7def
ticktack-st <moglandwrapper@gmail.com>
2025-06-12 20:25:37 +0900

    update; middlewareでリクエストのログ取得

--- app/src/middleware.ts
+++ app/src/middleware.ts
@@ -1,14 +1,75 @@
 import { runWithAmplifyServerContext } from '@/lib/amplifyServerUtils'
+import { logger } from '@/lib/logger'
 import { fetchAuthSession } from 'aws-amplify/auth/server'
-import { NextRequest, NextResponse } from 'next/server'
+import { NextRequest, NextResponse, userAgent } from 'next/server'
+import { v4 as uuidv4 } from 'uuid'
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    '/((?!api|_next/static|_next/image|favicon.ico).*)',
+  ],
+}
 
 export async function middleware(request: NextRequest) {
-  const hostname = request.headers.get('host') || ''
-  const subdomain = hostname.split('.')[0]
-  // テナント情報を request header や cookie に付与するなど
   const response = NextResponse.next()
+  const requestId = uuidv4()
+  response.headers.set('x-request-id', requestId)
 
-  const authenticated = await runWithAmplifyServerContext({
+  writeRequestInfoLog(request, requestId)
+
+  // 未ログイン時のリダイレクト先ページは除外する
+  if (request.nextUrl.pathname.startsWith('/login')) {
+    return response
+  }
+
+  // Cognito認証済みかどうかを確認
+  const authenticated = await confirmAuthenticated(request, response)
+  logger.info({
+    message: 'Authentication check completed',
+    authenticated: authenticated,
+  })
+
+  // 認証済みであれば、リクエストをそのまま通す
+  if (authenticated) {
+    return response
+  }
+
+  // 認証されていない場合は、ログインページにリダイレクト
+  return NextResponse.redirect(new URL('/login', request.url))
+}
+
+// private ///////////////////
+
+function writeRequestInfoLog(request: NextRequest, requestId: string) {
+  const agent = userAgent(request)
+  const requestInfo = {
+    ip: request.headers.get('x-forwarded-for') ?? '',
+    method: request.method,
+    pathname: request.nextUrl.pathname,
+    searchParams: Object.fromEntries(request.nextUrl.searchParams),
+    device: agent.device,
+    browser: agent.browser,
+    userAgent: request.headers.get('user-agent'),
+  }
+  logger.info({
+    message: 'middleware started',
+    requestId: requestId,
+    requestInfo,
+  })
+}
+
+async function confirmAuthenticated(
+  request: NextRequest,
+  response: NextResponse
+): Promise<boolean> {
+  return runWithAmplifyServerContext({
     nextServerContext: { request, response },
     operation: async (contextSpec) => {
       try {
@@ -20,61 +81,9 @@ export async function middleware(request: NextRequest) {
           session.tokens?.idToken !== undefined
         )
       } catch (error) {
-        console.log(error)
+        logger.warn({ message: 'Error fetching auth session', error })
         return false
       }
     },
   })
-  console.log('Authenticated_middle:', authenticated)
-
-  // // テナント情報を request header や cookie に付与するなど
-  // const response = NextResponse.next()
-  // if (subdomain !== 'localhost:3000') {
-  //   response.cookies.set('tenant', subdomain || '')
-  // } else if (subdomain === 'test') {
-  if (
-    subdomain === 'test' &&
-    request.nextUrl.pathname.startsWith('/logged-in')
-  ) {
-    return NextResponse.redirect(
-      new URL('https://test.test.localhost:3000/file-upload', request.url)
-    )
-  }
-
-  // const authenticated = await runWithAmplifyServerContext({
-  //   nextServerContext: nextContext,
-  //   operation: async (contextSpec) => {
-  //     try {
-  //       const session = await fetchAuthSession(contextSpec, {
-  //         forceRefresh: true,
-  //       })
-  //       return (
-  //         session.tokens?.accessToken !== undefined &&
-  //         session.tokens?.idToken !== undefined
-  //       )
-  //     } catch (error) {
-  //       return false
-  //     }
-  //   },
-  // })
-  // console.log('Authenticated_middle:', authenticated)
-
-  if (authenticated) {
-    return response
-  }
-
-  return NextResponse.redirect(new URL('/login', request.url))
-}
-
-export const config = {
-  matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - api (API routes)
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
-     */
-    '/((?!api|_next/static|_next/image|favicon.ico|login|file-upload).*)',
-  ],
 }
