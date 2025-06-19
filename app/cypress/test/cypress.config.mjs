23eb5b05d44e51b28f7081d57a259b1878839c00
ticktack-st <moglandwrapper@gmail.com>
2025-06-19 17:20:13 +0900

    fix: 環境変数の参照

--- app/cypress/test/cypress.config.mjs
+++ app/cypress/test/cypress.config.mjs
@@ -2,7 +2,14 @@ import { defineConfig } from 'cypress'
 
 export default defineConfig({
   e2e: {
-    baseUrl: process.env('BASE_URL') || 'http://localhost:3000',
+    setupNodeEvents(on, config) {
+      // ローカル用環境変数CYPRESS_BASE_URLが設定してある場合はそちらを使用
+      if (process.env.CYPRESS_BASE_URL) {
+        config.baseUrl = process.env.CYPRESS_BASE_URL
+      }
+      return config
+    },
+    baseUrl: 'http://localhost:3000',
     downloadsFolder: '/results/downloads',
     screenshotsFolder: '/results/screenshots',
     videosFolder: '/results/videos',
