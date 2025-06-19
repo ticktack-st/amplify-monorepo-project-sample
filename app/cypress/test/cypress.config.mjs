f5c9f82c813d3f57bbc46e51281a495f63dd9876
ticktack-st <moglandwrapper@gmail.com>
2025-06-19 18:19:31 +0900

    fix: 修正ミス

--- app/cypress/test/cypress.config.mjs
+++ app/cypress/test/cypress.config.mjs
@@ -6,17 +6,17 @@ export default defineConfig({
       // ローカル用環境変数CYPRESS_BASE_URLが設定してある場合はそちらを使用
       if (process.env.CYPRESS_BASE_URL) {
         ;(config.baseUrl = process.env.CYPRESS_BASE_URL),
-          (config.downloadsFolder = 'results/downloads'),
-          (config.screenshotsFolder = 'results/screenshots'),
-          (config.videosFolder = 'results/videos'),
+          (config.downloadsFolder = '/results/downloads'),
+          (config.screenshotsFolder = '/results/screenshots'),
+          (config.videosFolder = '/results/videos'),
           (config.specPattern = 'src/**/*.cy.ts')
       }
       return config
     },
     baseUrl: 'http://localhost:3000',
-    downloadsFolder: '/results/downloads',
-    screenshotsFolder: '/results/screenshots',
-    videosFolder: '/results/videos',
+    downloadsFolder: 'results/downloads',
+    screenshotsFolder: 'results/screenshots',
+    videosFolder: 'results/videos',
     video: true,
     videoCompression: false,
     trashAssetsBeforeRuns: true,
